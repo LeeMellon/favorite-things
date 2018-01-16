a63e7a86273c388bb978074ae6348afba9932c11
Cameron Anderson and Ian Goodrich <cameron@>
2018-01-16 08:57:12 -0800

    play with arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,21 @@
 $(document).ready(function() {
+
+
+
+  $(".form1").submit(function(event){
+    event.preventDefault();
+
+    var ourArray= [];
+
+    ourArray.push($("#input1").val());
+    ourArray.push($("#input2").val());
+    ourArray.push($("#input3").val());
+
+    var newArray = [];
+    newArray.push(ourArray[1], ourArray[2], ourArray[0]);
+
+    $("#list").append("<li>" + newArray.toString() + "</li>");
+
+  });
+
 });
